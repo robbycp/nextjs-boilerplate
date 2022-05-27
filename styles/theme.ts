fed071c446752e9e97744bc3d14f39d762b8ef57
robby <dev.robbycaesar@gmail.com>
2022-05-27 15:30:45 +0700

    [MODIFY] reorganize theme token

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,19 +1,9 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import color from './token/color'
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-  },
+  palette: { ...color },
 });
 
 export default theme;
